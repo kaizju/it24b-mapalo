c6dd497a0188f68a231e793cf2a60f4d5ceba53f
precellemae <precellemaenieze@gmail.com>
2024-10-11 09:44:33 +0800

    added constructor

--- Event Logger/applet1.js
+++ Event Logger/applet1.js
@@ -1,27 +1,13 @@
 class LogTracker {
     constructor(totallogsElement, logButton, logContainer) {
-      this.totallogs = 0;
-      this.totallogsElement = totallogsElement;
-      this.logButton = logButton;
-      this.logContainer = logContainer;
-      this.logButton.addEventListener('click', this.logButtonClick.bind(this));
-    }
-  
-    logButtonClick() {
-      const currentDate = new Date();
-      const logMessage = `Logged on ${currentDate.toLocaleString()}`;
-      this.logContainer.innerHTML += `<p>${logMessage}</p>`;
-      this.totallogs++;
-      this.totallogsElement.textContent = `Total logs: ${this.totallogs}`;
-    }
+        this.totallogs = 0;
+        this.totallogsElement = totallogsElement;
+        this.logButton = logButton;
+        this.logContainer = logContainer;
+        this.logButton.addEventListener('click', this.logButtonClick.bind(this));
+      }
   
 }
-  
-  const totallogsElement = document.getElementById('total-logs');
-  const logButton = document.getElementById('log-button');
-  const logContainer = document.getElementById('log-container');
-  
-  const logTracker = new LogTracker(totallogsElement, logButton, logContainer);
   function updateDisplay() {
     const textbox = document.getElementById('textbox');
     const displayText = document.getElementById('displayText');
